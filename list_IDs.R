75354be3f1a3a864691e44cda4fc8865ea455e74
megshep <142318637+megshep@users.noreply.github.com>
2025-09-26 12:18:45 +0100

    Create list_IDs.R
    This generated a list of IDs for those who have data from one the timepoints

---
+++ list_IDs.R
@@ -0,0 +1,28 @@
+# Load the required library
+library(openxlsx)
+
+# Define the path to your folder with the files
+folder_path <- "S:/Meg_Stuff/FU3_nifti"  # <-- Replace with your actual path
+
+# List all files in the folder (no need for full names)
+files <- list.files(folder_path, full.names = FALSE)
+
+# Remove extensions (.nii.gz, .nii) for all files
+ids <- sub("\\.nii\\.gz$|\\.nii$", "", files)
+
+# Remove any duplicates (to ensure each ID is listed only once, .nii.gz and .nii both present)
+unique_ids <- unique(ids)
+# Create a data frame with the unique IDs (without file extensions) - this is important as I have each files zipped and unzipped in the folder, so don't want repeats and don't want extensions
+id_df <- data.frame(Subject_ID = unique_ids, stringsAsFactors = FALSE)
+
+# Define the output file name
+file_name <- paste0("subject_ids_FU3_unique_p2", ".xlsx")
+
+# Set the output Excel file path
+output_excel <- file.path("S:/Meg_Stuff/FU3_nifti", file_name)
+
+# Write the data frame to Excel
+write.xlsx(id_df, output_excel, rowNames = FALSE)
+
+# Done
+cat("Excel file created at:", output_excel, "\n")
