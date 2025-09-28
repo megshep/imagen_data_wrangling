5ba23a4c2b3755a07207f159e5cfe93065eac538
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-09-28 21:25:27 +0100

    Update removing_discrepancies_between_timepoints.R

--- removing_discrepancies_between_timepoints.R
+++ removing_discrepancies_between_timepoints.R
@@ -1,14 +1,13 @@
-# Load required libraries
 library(openxlsx)
 library(fs)
 library(stringr)
 
-# Read valid subject IDs from Excel
+# Read valid subject IDs from Excel, assuming that there is a column titled 'Common_Subject_IDs'
 subject_ids_path <- "S:/Meg_Stuff/subject_ids_common_p2.xlsx"
 subject_ids_df <- read.xlsx(subject_ids_path)
 valid_ids <- tolower(str_trim(subject_ids_df$Common_Subject_IDs))
 
-# Define source folders and corresponding extras subfolders
+# Define source folders and corresponding extras subfolders - this is so that there is a clear folder to move any spare files to
 folder_info <- list(
   FU1_nifti = list(path = "S:/Meg_Stuff/FU2_nifti", extras_sub = "FU2_Extras"),
   FU2_nifti = list(path = "S:/Meg_Stuff/FU3_nifti", extras_sub = "FU3_Extras"),
@@ -19,7 +18,7 @@ folder_info <- list(
 base_extras <- "S:/Meg_Stuff/extras"
 dir_create(base_extras)
 
-# Function to move unmatched files to appropriate subfolder
+# Function to move unmatched files to the appropriate subfolder
 move_extras <- function(folder_path, extras_subfolder, valid_ids, base_extras) {
   files <- list.files(folder_path, full.names = TRUE)
   subfolder_path <- file.path(base_extras, extras_subfolder)
@@ -47,4 +46,4 @@ for (folder_name in names(folder_info)) {
   move_extras(path, extras_sub, valid_ids, base_extras)
 }
 
-cat("âœ… Done! Unmatched files sorted into extras subfolders.\n")
+cat("Done. Unmatched files sorted into extras subfolders.\n")
