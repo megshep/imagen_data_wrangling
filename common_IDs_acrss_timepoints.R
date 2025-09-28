e861412399238029f08fd55562d29de809716b67
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-09-28 20:48:45 +0100

    Update common_IDs_acrss_timepoints.R
    More in-depth comments added

--- common_IDs_acrss_timepoints.R
+++ common_IDs_acrss_timepoints.R
@@ -1,17 +1,16 @@
-# Load required packages
 library(openxlsx)
 
-# Paths to your 3 Excel files
+# Paths to the 3 Excel files (this instance its an excel file for each timepoint of my analysis)
 file1 <- "S:/Meg_Stuff/BL_nifti/subject_ids_BL_unique_p2.xlsx"
 file2 <- "S:/Meg_Stuff/FU2_nifti/subject_ids_FU2_unique_p2.xlsx"
 file3 <- "S:/Meg_Stuff/FU3_nifti/subject_ids_FU3_unique_p2.xlsx"
 
-# Read all three Excel sheets (assuming they all have a column called "Subject_ID")
+# Read all three Excel sheets (assuming they all have a column called "Subject_ID" - this needs to be checked beforehand or it will fail!)
 ids1 <- read.xlsx(file1)$Subject_ID
 ids2 <- read.xlsx(file2)$Subject_ID
 ids3 <- read.xlsx(file3)$Subject_ID
 
-# Find common IDs across all three
+# Find common IDs across all three Excel sheets
 common_ids <- Reduce(intersect, list(ids1, ids2, ids3))
 
 # Create a data frame of the overlapping IDs
