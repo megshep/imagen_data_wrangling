9c35d32223b7cedf5f0f969a869d2eeb4ede41dc
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-09-28 21:21:08 +0100

    Update list_IDs.R

--- list_IDs.R
+++ list_IDs.R
@@ -1,16 +1,15 @@
-# Load the required library
 library(openxlsx)
 
 # Define the path to your folder with the files
-folder_path <- "S:/Meg_Stuff/FU3_nifti"  # <-- Replace with your actual path
+folder_path <- "S:/Meg_Stuff/FU3_nifti"  
 
 # List all files in the folder (no need for full names)
 files <- list.files(folder_path, full.names = FALSE)
 
-# Remove extensions (.nii.gz, .nii) for all files
+# Remove extensions (.nii.gz, .nii) for all files as I just want a list of the IDs themselves and not the file names, skip this step if you want file names
 ids <- sub("\\.nii\\.gz$|\\.nii$", "", files)
 
-# Remove any duplicates (to ensure each ID is listed only once, .nii.gz and .nii both present)
+# Remove any duplicates (to ensure each ID is listed only once, as .nii.gz and .nii are both present)
 unique_ids <- unique(ids)
 # Create a data frame with the unique IDs (without file extensions) - this is important as I have each files zipped and unzipped in the folder, so don't want repeats and don't want extensions
 id_df <- data.frame(Subject_ID = unique_ids, stringsAsFactors = FALSE)
