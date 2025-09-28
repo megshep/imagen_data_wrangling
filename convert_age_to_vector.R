ab4b650636eb1990ab5ec02b2d7f9b5108110b09
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-09-28 20:51:23 +0100

    Update convert_age_to_vector.R
    More in depth comments added

--- convert_age_to_vector.R
+++ convert_age_to_vector.R
@@ -1,11 +1,12 @@
+#Read in the csv file that has the ages calculated and converted into years
 ages <- read.csv("vector_imputed_ages_years.csv")
 
-# Compute times relative to baseline
+# Compute times relative to baseline (how far away from baseline are the two follow-up scans for each participant?)
 rel_times <- ages
 rel_times$Age_FU2 <- rel_times$Age_FU2 - rel_times$Age_BL
 rel_times$Age_FU3 <- rel_times$Age_FU3 - rel_times$Age_BL
 rel_times$Age_BL <- 0
 
-# Optional: save as a long format for MATLAB if needed
-# Or just keep wide format with baseline, FU2, FU3
+
+# save as a .csv file with the vector information ready to be input into the model
 write.csv(rel_times, "vector_relative_times_years.csv", row.names = FALSE)
