db506dbbbb722765156781a000ff2305017a3afe
Meg Sheppard <142318637+megshep@users.noreply.github.com>
2025-09-28 20:30:25 +0100

    Update MRI_download_BL_imagen.R
    more in depth code comments throughout

--- MRI_download_BL_imagen.R
+++ MRI_download_BL_imagen.R
@@ -1,39 +1,38 @@
-# FTP server details
+#Server information - this is the log in information and the server url itself
 ftp_user <- "msheppard"
 ftp_password <- "[password]"
-ftp_url <- "sftp://imagen2.cea.fr"  # FTP URL
+ftp_url <- "sftp://imagen2.cea.fr"  # FTP URL (this is the specific one for IMAGEN]
 
-# Specify the local path to the CSV file
-csv_file_path <- "S:/Meg_Stuff/BL_nifti/CTQ_FU2.csv"  # Path to the CSV file with user codes
+# Specify the local path to the CSV file 
+csv_file_path <- "S:/Meg_Stuff/BL_nifti/CTQ_FU2.csv"  # Path to the CSV file with the participant IDs
 
-
-# Read the CSV file
+# Read the CSV file 
 data <- read.csv(csv_file_path)
 
-# Extract the 'User code' column 
-user_codes <- data$`User.code`  # Use backticks if the column has spaces
+# This is the Participant IDs column - in the imagen database it's in a column titled 'User.code' 
+user_codes <- data$`User.code` 
 
-# Local directory where you want to save the files
-local_directory <- "S:/Meg_Stuff/BL_nifti"      # Local directory to save downloaded files
+# Local directory where you want to save the files (output directory)
+local_directory <- "S:/Meg_Stuff/BL_nifti"      
 
 # Loop through each user code and download the matching file
 for (user_code in user_codes) {
   
-  #remove alphabetic letter and hyphen from the user code
+  #remove alphabetic letter and hyphen from the user code (all of them had -I in them for some reason, so this needed to be removed first)
   user_code <- gsub("[^0-9]", "", user_code)
   
-  # Construct the remote file path 
+  # This says look in this path - this is important because in the IMAGEN database, the scans are saved in folders named under their Participant ID so this needs to be built into the pathway
   remote_file <- paste0("/data/imagen/2.7/BL/imaging/nifti/",user_code, "/SessionA/ADNI_MPRAGE/")
   
-  # Local file path
+  # Creates a local file path to save the files in the directory specified above as their participant ID.nii.gz e.g., 000000126629162.nii.gz
   local_file <- file.path(local_directory, paste0(user_code, ".nii.gz"))
   
-  # Construct FTP URL with authentication, using the URL-encoded password
+  # This logs into the FTP URL with the authentication information (username, password above)
   ftp_full_url <- paste0(ftp_url, remote_file)
-  
+
+  # Get a list of files in the directory of the user code using system call to curl with -k parameter  (make sure path to curl is correct! this has to be saved somewhere on the local computer!)
   getListOfFilesCommand <- paste("c:/curl/bin/curl.exe --insecure -k --user", paste0(ftp_user, ":", ftp_password), paste0("sftp://imagen2.cea.fr/data/imagen/2.7/BL/imaging/nifti/",user_code,"/SessionA/ADNI_MPRAGE/"))
   
-  # Get a list of files in the directory of the user code using system call to curl with -k parameter  (make sure path to curl is correct!)
   directory_listing <- system(getListOfFilesCommand, intern = TRUE)
   
   filenames <- sapply(directory_listing, function(line) {
@@ -50,10 +49,11 @@ for (user_code in user_codes) {
       remote_file <- paste0("/data/imagen/2.7/BL/imaging/nifti/",user_code,"/SessionA/ADNI_MPRAGE/", f)
       ftp_full_url <- paste0(ftp_url, remote_file)
       
-      # Build the curl command for the system call to download the file (make sure path to curl is correct!)
+      # Build the curl command for the system call to download the file (make sure the path to curl is correct!)
       #curl_command <- paste("c:/curl/bin/curl.exe --insecure --verbose --user", paste0(ftp_user, ":", ftp_password), ftp_full_url)  # Remote URL to download from
       
-      # Build the curl command for the system call to download the file (make sure path to curl is correct!)
+      # Build the curl command for the system call to download the file (make sure the path to curl is correct!). This includes a 60-second blocker to prevent server throttling
+      # Do not remove this or the files won't download and there will be server throttling
       curl_command <- paste(
         "c:/curl/bin/curl.exe --insecure --verbose --user", 
         paste0(ftp_user, ":", ftp_password), 
@@ -71,7 +71,7 @@ for (user_code in user_codes) {
     }
   }
   
-  #Add a 60 second delay between downloads to prevent server throttling
+  #This produces the 60 second delay and also provides a comment to tell you it's waiting for x amount of seconds
   for (i in 60:1) {
     cat("Waiting", i, "seconds...\r")  # \r returns cursor to start of line
     flush.console()                    # Ensures immediate output
